316e0338dfa6e8f6ea7f299434b2f856bb26429c
joshua <wanhoyinjoshua@yahoo.comgit config --global user.name wanhoyinjoshuagit config --global user.name wanhoyinjoshua>
2022-10-01 23:34:51 +1000

    hi

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,13 +1,21 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
+import React, { useRef, useState } from "react";
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 import { useUser } from '@auth0/nextjs-auth0';
 import { getAccessToken } from '@auth0/nextjs-auth0';
 import Link from "next/link"
-
+import { Swiper, SwiperSlide } from 'swiper/react';
+import { Navigation, Pagination, Scrollbar, A11y } from 'swiper';
+import 'swiper/css';
+import { useSwiper } from 'swiper/react';
+import ReactSwipe from 'react-swipe';
 const Home: NextPage = () => {
-  const { user } = useUser();
+  let reactSwipeEl: ReactSwipe | null;
+ 
+ 
+
 
   return (
     <>
@@ -18,7 +26,27 @@ const Home: NextPage = () => {
       </Head>
     <Link href="/api/auth/login">Login</Link>
     <input type="file" accept="image/*" capture="environment"/>
-    
+    <ReactSwipe
+        
+        swipeOptions={{ continuous: false }}
+        ref={el => (reactSwipeEl = el)}
+      >
+       <div>
+            <img src="http://placekitten.com/g/400/200" />
+          </div>
+          <div>
+            <img src="http://placekitten.com/g/400/200" />
+          </div>
+          <div>
+            <img src="http://placekitten.com/g/400/200" />
+          </div>
+          <div>
+            <img src="http://placekitten.com/g/400/200" />
+          </div>
+      </ReactSwipe>
+
+      <button onClick={() => {if(reactSwipeEl){reactSwipeEl.next()}}}>Next</button>
+      <button onClick={() => {if(reactSwipeEl){reactSwipeEl.prev()}}}>Previous</button>
   
     </>
    
